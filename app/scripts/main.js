3d38f3ee69ecef149331b31079f20338da6de37a
Nick Echols <stere.ereo@gmail.com>
2016-05-09 00:37:36 -0700

    Visual tweaks

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,84 +1,91 @@
+'use strict';
 
-  'use strict';
+var showJumbotron = true;
+var latestKnownScrollY = 0;
+var ticking = false;
 
-  var showJumbotron = true;
-  var latestKnownScrollY = 0;
-  var ticking = false;
-
-  window.onscroll = function() {
-    latestKnownScrollY = window.scrollY;
-    requestTick();
+var hideJumbo = function() {
+  var header = document.querySelector('header');
+  var spacer = document.querySelector('.spacer');
+  spacer.style.display = 'block';
+  if (header.classList) {
+    header.classList.add('small-header');
+  } else {
+    header.className += ' ' + 'small-header';
   }
+  showJumbotron = false;
+  document.querySelector('.close-icon').textContent = '+';
+};
 
-  function requestTick() {
-    if(!ticking) {
-      requestAnimationFrame(update);
-    }
-    ticking = true;
+var showJumbo = function() {
+  var header = document.querySelector('header');
+  var spacer = document.querySelector('.spacer');
+  
+  if (header.classList) {
+    header.classList.remove('small-header');
+  } else {
+    header.className = header.className.replace(new RegExp('(^|\\b)' + 'small-header'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
   }
-  function update() {
-    ticking = false;
-    var currentScrollY = latestKnownScrollY;
-    if (currentScrollY > 30 && showJumbotron) {
-    
-    }
-  }
+  showJumbotron = true;
+  document.querySelector('.close-icon').textContent = 'x';
+  spacer.style.display = 'none';
+};
 
-  function hideJumbo() {
-     var jt = document.querySelector('.jumbotron')
-      if (jt.classList)
-        jt.classList.add('slideup');
-      else {
-        jt.className += ' ' + 'slideup';
-      }
-      showJumbotron = false;
-      document.querySelector('.close-icon').textContent = '+';
+var toggleJumbo = function() {
+  if (showJumbotron) {
+    hideJumbo();
+  } else {
+    showJumbo();
   }
-  function showJumbo() {
-    var jt = document.querySelector('.jumbotron')
-    if (jt.classList)
-      jt.classList.remove('slideup');
-    else
-      jt.className = jt.className.replace(new RegExp('(^|\\b)' + 'slideup'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-    showJumbotron = true;
-    document.querySelector('.close-icon').textContent = 'x';
-  }
+};
 
-  function toggleJumbo() {
-    if (showJumbotron) {
-      hideJumbo();
-    } else {
-      showJumbo();
-    }
+var scrollUpdate = function() {
+  ticking = false;
+  var currentScrollY = latestKnownScrollY;
+  if (currentScrollY > 30 && showJumbotron) {
+    hideJumbo();
   }
+};
 
-document.addEventListener("DOMContentLoaded", function() {
-   document.querySelector('.close-icon').addEventListener('click', toggleJumbo);
+var requestTick = function() {
+  if (!ticking) {
+    requestAnimationFrame(scrollUpdate);
+  }
+  ticking = true;
+};
+
+window.onscroll = function() {
+  latestKnownScrollY = window.scrollY;
+  requestTick();
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('.close-icon').addEventListener('click', toggleJumbo);
 });
- 
 
-  // $(document).ready(function() {
-  //   $('#fullpage').fullpage({
-  //     // sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE']
-  //   });
-  // });
 
-  // $('.svg-draw path').each(function() {
-  //   initSvg(this);
-  // });
+// $(document).ready(function() {
+//   $('#fullpage').fullpage({
+//     // sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE']
+//   });
+// });
 
-  // $('.svg-draw').on('mouseenter', function() {
-  //   $(this).find('path').each(function() {
-  //     svgAnimation(this, 'in');
-  //   });
-  // });
-  // $('.svg-draw').on('mouseleave', function() {
-  //   $(this).find('path').each(function() {
-  //     svgAnimation(this, 'out');
-  //   });
-  // });
-  // $('.scroll-down').click(function(e) {
-  //   e.preventDefault();
-  //   $.fn.fullpage.moveSectionDown();
-  // });
+// $('.svg-draw path').each(function() {
+//   initSvg(this);
+// });
+
+// $('.svg-draw').on('mouseenter', function() {
+//   $(this).find('path').each(function() {
+//     svgAnimation(this, 'in');
+//   });
+// });
+// $('.svg-draw').on('mouseleave', function() {
+//   $(this).find('path').each(function() {
+//     svgAnimation(this, 'out');
+//   });
+// });
+// $('.scroll-down').click(function(e) {
+//   e.preventDefault();
+//   $.fn.fullpage.moveSectionDown();
+// });
 // })();
