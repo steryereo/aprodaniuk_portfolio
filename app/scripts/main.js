3fb7ea232a0b22654ca95f982f288c6bbd4bae49
Nick Echols <stere.ereo@gmail.com>
2016-05-12 23:55:28 -0700

    Add sticky header with parallax

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,22 @@
 var showJumbotron = true;
 var latestKnownScrollY = 0;
 var ticking = false;
+var lgHeaderHeight = 345 - 68;
+var headerStuck = false;
+
+var colorIndex = 0;
+var colors = ['#595959', '#3cd3c4', '#747b6a', '#b89c2f', '#321F39', '#ef6c48'];
+var changeColor = function (color) {
+  var colorchange = document.querySelectorAll('.color-change');
+  var colorchangestroke = document.querySelectorAll('.color-change-stroke');
+  [].forEach.call(colorchange, function(el) {
+    el.setAttribute('fill', color);
+  });
+  [].forEach.call(colorchangestroke, function(el) {
+    el.setAttribute('stroke', color);
+  });
+};
+
 
 var addClass = function(el, className) {
   if (el.classList) {
@@ -51,19 +67,35 @@ var showDescription = function(e) {
   var infoIcon = document.querySelector('.info-icon');
   addClass(infoIcon, 'fade-out');
 };
-var toggleJumbo = function() {
-  if (showJumbotron) {
-    hideJumbo();
-  } else {
-    showJumbo();
-  }
-};
 
+var stickHeader = function (header) {
+  console.log('stick');
+  addClass(header, 'fixed-header');
+  header.setAttribute('style', 'opacity: 1;');
+  headerStuck = true;
+};
+var unstickHeader = function (header) {
+  removeClass(header, 'fixed-header');
+  headerStuck = false;
+  console.log('unstick');
+};
 var scrollUpdate = function() {
   ticking = false;
   var currentScrollY = latestKnownScrollY;
-  if (currentScrollY > 30 && showJumbotron) {
-    hideJumbo();
+  var headerSm = document.querySelector('.header-sm');
+  var headerLg = document.querySelector('.header-lg');
+  if (headerLg && headerSm) {
+    if (currentScrollY <= lgHeaderHeight) {
+      if (headerStuck) {
+        unstickHeader(headerSm);
+      }
+      var opacity = (currentScrollY / lgHeaderHeight);
+      headerSm.setAttribute('style', 'opacity: ' + (opacity * opacity * opacity).toFixed(2) + ';');
+      headerLg.setAttribute('style', 'transform: translateY(-' + (opacity * 68).toFixed(0) + 'px);');
+      // headerLg.setAttribute('style', 'opacity: ' + (1 - opacity).toFixed(2) + ';');
+    } else if (!headerStuck) {
+      stickHeader(headerSm);
+    }
   }
 };
 
@@ -79,53 +111,34 @@ var requestTick = function() {
  * Provides requestAnimationFrame in a cross browser way.
  * @author paulirish / http://paulirish.com/
  */
-if ( !window.requestAnimationFrame ) {
-  window.requestAnimationFrame = ( function() {
+if (!window.requestAnimationFrame) {
+  window.requestAnimationFrame = (function() {
     return window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    window.oRequestAnimationFrame ||
-    window.msRequestAnimationFrame ||
-    function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element ) {
-      window.setTimeout( callback, 1000 / 60 );
-    };
-  } )();
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+        window.setTimeout(callback, 1000 / 60);
+      };
+  })();
 }
 
-window.onscroll = function() {
+window.onscroll = function(e) {
   latestKnownScrollY = window.scrollY;
+  // console.log(latestKnownScrollY);
   if (showJumbotron) {
     requestTick();
   }
 };
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('header .close-icon').addEventListener('click', toggleJumbo);
-  document.querySelector('.main-image .close-icon').addEventListener('click', hideDescription);
-  document.querySelector('.main-image .info-icon').addEventListener('click', showDescription);
+  // document.querySelector('header .close-icon').addEventListener('click', toggleJumbo);
+  var infoHide = document.querySelector('.main-image .close-icon');
+  if (infoHide) { infoHide.addEventListener('click', hideDescription); }
+  var infoShow = document.querySelector('.main-image .info-icon');
+  if (infoShow) { infoShow.addEventListener('click', showDescription); }
+  setInterval(function() {
+    changeColor(colors[colorIndex]);
+    colorIndex = (colorIndex + 1) % colors.length;
+  }, 10000);
 });
-
-// $(document).ready(function() {
-//   $('#fullpage').fullpage({
-//     // sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE']
-//   });
-// });
-
-// $('.svg-draw path').each(function() {
-//   initSvg(this);
-// });
-
-// $('.svg-draw').on('mouseenter', function() {
-//   $(this).find('path').each(function() {
-//     svgAnimation(this, 'in');
-//   });
-// });
-// $('.svg-draw').on('mouseleave', function() {
-//   $(this).find('path').each(function() {
-//     svgAnimation(this, 'out');
-//   });
-// });
-// $('.scroll-down').click(function(e) {
-//   e.preventDefault();
-//   $.fn.fullpage.moveSectionDown();
-// });
-// })();
